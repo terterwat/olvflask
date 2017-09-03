5597fff13713a1c4499068ddb0027f747a1d2be0
ufo <udo.storz@gmail.com>
2017-09-03 10:57:15 +0200

    first chang

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,17 @@
+#!/usr/bin/env python
+## -*- coding: utf-8 -*-
+import os
+
 from flask import Flask
 application = Flask(__name__)
 
+import cherrypy
+from mako.template import Template
+from mako.lookup import TemplateLookup
+
 @application.route("/")
 def hello():
-    return "Hello Schnute!"
+    return "Hello Mako!"
 
 if __name__ == "__main__":
     application.run()
